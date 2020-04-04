5800df151a98e5224c7a9282997cd3dd0dad8454
Catherine Palermo <cspalermo@hotmail.com>
2020-04-04 13:53:05 -0700

    solved problem 2

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    var sum = a + b;
-    return [sum, 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.'];
+  var sum = a + b;
+  return [sum, 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.'];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -28,11 +28,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  var multiply = a * b;
+  return [multiply, 'The product of ' + a + ' and ' + b + ' is ' + multiply + '.'];
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
